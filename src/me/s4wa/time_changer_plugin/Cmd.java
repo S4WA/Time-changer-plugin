8013b87d167482434662f07448cdb08d7fa88176
Miyazawa <contact@s4wa.me>
2018-05-08 00:48:48 +0900

    Update Cmd.java

--- src/me/s4wa/time_changer_plugin/Cmd.java
+++ src/me/s4wa/time_changer_plugin/Cmd.java
@@ -11,14 +11,15 @@ public class Cmd implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player player = (Player) sender;
 
-		if (args.length == 0) {
-			sender.sendMessage("/" + label + " <day, noon, night, reset>");
-		} else if (args.length == 1) {
+		if (args.length == 0) return false;
+		else if (args.length == 1) {
 			if (args[0].equalsIgnoreCase("day")) player.setPlayerTime(1000, false);
 			else if (args[0].equalsIgnoreCase("noon")) player.setPlayerTime(6000, false);
 			else if (args[0].equalsIgnoreCase("night")) player.setPlayerTime(18000, false);
 			else if (args[0].equalsIgnoreCase("reset")) player.resetPlayerTime();
-			else if (args[0] != null) player.setPlayerTime(Long.valueOf(args[0]), false);
+			else if (args[0] != null && Long.valueOf(args[0]) != null)
+				player.setPlayerTime(Long.valueOf(args[0]), false);
+			else return false;
 		}
 		return true;
 	}
